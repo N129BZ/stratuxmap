fd62a5eeb67cc27177adda6847aa8515bb5c2e69
n129bz <n129bz@outlook.com>
2025-09-20 19:34:09 -0500

    Remove touch event handlers for swipe navigation in map.js

--- src/map.js
+++ src/map.js
@@ -99,8 +99,6 @@ let closeButton = {};
 let popup = {};
 let popupcontent = {};
 let airplaneElement = {};
-let touchStartX = 0;
-let touchEndX = 0;
 
 document.addEventListener('DOMContentLoaded', async function () {
     // Wait for mapsettings to be loaded before initializing anything that depends on it
@@ -240,27 +238,6 @@ document.addEventListener('DOMContentLoaded', async function () {
         }
     });
 
-    // Touch event handlers for swipe navigation
-    document.addEventListener('touchstart', function(e) {
-        touchStartX = e.changedTouches[0].screenX;
-    }, false);
-
-    document.addEventListener('touchend', function(e) {
-        touchEndX = e.changedTouches[0].screenX;
-        handleGesture();
-    }, false);
-
-    function handleGesture() {
-        if (touchEndX < touchStartX - mapsettings.swipeThreshold) {
-            // Left swipe: go forward
-            window.history.forward();
-        }
-        if (touchEndX > touchStartX + mapsettings.swipeThreshold) {
-            // Right swipe: go back
-            window.history.back();
-        }
-    }
-    // End of touch event handlers
 
     /* Map objects used for various keyname lookups */
     let airportNameKeymap = new Map();
